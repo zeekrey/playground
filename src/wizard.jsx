038ebcee36c4c73037c8c487b42674462314c136
christian@krey.io <christian@krey.io>
2020-06-03 05:54:19 +0200

    Added wizard.

--- src/wizard.jsx
+++ src/wizard.jsx
@@ -4,6 +4,54 @@ import React from 'react'
  * This is me trying to build a step wizard.
  */
 
-export const Wizard = () => {
-    return <div>wizard</div>
+const Step = ({ children }) => {
+    return <div>{children}</div>
+}
+
+const WizardContainer = ({ children }) => {
+    const stepCount = React.Children.toArray(children).filter(
+        (child) => child.type.name === 'Step'
+    ).length
+
+    React.useEffect(
+        () =>
+            console.log(
+                React.Children.toArray(children).map((child, index) => {
+                    return {
+                        id: index,
+                        component: child,
+                        name: child.props.name,
+                        props: { ...child.props },
+                    }
+                })
+            ),
+        []
+    )
+
+    const [step, setStep] = React.useState(0)
+    return (
+        <div>
+            <div>Current step: {step}</div>
+            <div>Max steps: {stepCount}</div>
+
+            <div>{children[step]}</div>
+
+            {step > 0 && (
+                <button onClick={() => setStep(step - 1)}>Previous</button>
+            )}
+            {stepCount - 1 > step && (
+                <button onClick={() => setStep(step + 1)}>Next</button>
+            )}
+        </div>
+    )
+}
+
+export const Wizard = ({ children }) => {
+    return (
+        <WizardContainer>
+            <Step>This is step 1</Step>
+            <Step>This is step 2</Step>
+            <Step>This is step 3</Step>
+        </WizardContainer>
+    )
 }
